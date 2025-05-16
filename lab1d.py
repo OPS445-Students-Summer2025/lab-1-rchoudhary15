ee00eb827212233df2ab2f7443ec5006cf1a9702
Raghav Choudhary <rchoudhary15@myseneca.ca>
2025-05-16 19:12:08 -0400

    Added lab1d

---
+++ lab1d.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+'''This script performs a math operation and prints the expression with the result.'''
+
+x = 10
+y = 2
+z = 5
+
+result = x + y * z
+print(str(x) + ' + ' + str(y) + ' * ' + str(z) + ' = ' + str(result))
